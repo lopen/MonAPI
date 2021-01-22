51616a961e91c6903299752bc0f5e93f41d51f7a
lopen <trygveulsnes@gmail.com>
2021-01-22 23:41:44 +0100

    Adding source files.

---
+++ src/main/java/com/trygveulsnes/MonAPI/MonApiApplication.java
@@ -0,0 +1,13 @@
+package com.trygveulsnes.MonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trygveulsnes/MonAPI/MonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trygveulsnes.MonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
